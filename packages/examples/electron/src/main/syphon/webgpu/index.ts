eb6480c591884678373b25230edb181d238e49ce
benoitlahoz <lahoz.ben@gmail.com>
2025-02-24 14:17:27 +0100

    Create index.ts

---
+++ packages/examples/electron/src/main/syphon/webgpu/index.ts
@@ -0,0 +1,5 @@
+// https://github.com/electron/electron/pull/42001#issuecomment-2089032620
+// https://github.com/electron/electron/pull/42001#issuecomment-2144319975
+// https://stackoverflow.com/questions/77411785/inrterop-between-webgl2-and-webgpu
+
+export const unimplemented = {};
